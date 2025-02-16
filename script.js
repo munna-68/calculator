8ed917a3a223cc2ed99808f5dd74bb3be86dbd1d
munna-68 <mahamudmunna68@gmail.com>
2025-02-17 01:45:40 +0600

    not much just writing the operate fucntion in js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("hello world")
+
+
+
+
+
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multipication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  if(operator === '+') return add(a, b);
+  if(operator === '-') return subtraction(a, b);
+  if(operator === '*') return multipication(a, b);
+  if(operator === '/') return division(a, b);
+}
