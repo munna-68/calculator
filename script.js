e898457626d4bdbea8e741a85704a07a5e4dfa95
munna-68 <mahamudmunna68@gmail.com>
2025-02-22 01:16:02 +0600

    adding the button funtionality
    when the button is clicked its beign showed on the top part of the display

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const buttons = document.querySelectorAll('button');
+const displayTextChain = document.querySelector('h3')
+let topDisplay = []
 
 
-
-
-
-
+console.log(topDisplay)
+buttons.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    topDisplay.push(event.target.textContent);
+    console.log(topDisplay)
+    displayTextChain.textContent = topDisplay.join('')
+  })
+})
 
 function add(a, b) {
   return a + b;
